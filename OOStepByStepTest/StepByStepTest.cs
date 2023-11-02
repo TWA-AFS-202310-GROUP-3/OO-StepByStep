32f5ef168c9dc34dbdd7f29c8b786e4ad483204a
Kailin Liu <KLiu13@slb.com>
2023-11-02 17:13:01 +0800

    feat: creat person class that can introduce itself

---
+++ OOStepByStepTest/StepByStepTest.cs
@@ -0,0 +1,26 @@
+﻿using OOStepByStep;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OOStepByStepTest
+{
+    public class StepByStepTest
+    {
+        [Fact]
+        public void Should_return_name_and_age()
+        {
+            //Given
+            Person person = new Person("Tom", 21);
+
+            //When
+            string introduction = person.Introduce();
+
+            //Then
+            Assert.Equal("My name is Tom. I am 21 years old.", introduction);
+        }
+    }
+}
