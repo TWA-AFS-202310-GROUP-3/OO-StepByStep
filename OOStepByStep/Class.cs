0a23db5da31c70f961906e81a542e4bf3018ac16
sjin9 <sjin9@slb.com>
2023-11-02 23:14:59 +0800

    fix: remove reduncant code and allow a class has one teacher and multiple student

--- OOStepByStep/Class.cs
+++ OOStepByStep/Class.cs
@@ -8,17 +8,40 @@ namespace OOStepByStep
 {
     public class Class
     {
-        private Person person;
+        // private Person person;
+        // private Teacher teacher;
+        private List<Student> students = new ();
+        private int classNumber;
 
-        public Class(Person person)
+        public Class(int classNumber)
         {
-            this.person = person;
+            this.classNumber = classNumber;
         }
 
-        public string ClassMember(Person person, int classNumber)
+        public string ClassMember(Person person)
         {
-            var memberIntroduction = person.Introduce();
-            return memberIntroduction.Substring(0, memberIntroduction.Length - 1) + " of class " + classNumber + ".";
+            if (person is Teacher)
+            {
+                // teacher = (Teacher)person;
+                var memberIntroduction = person.Introduce();
+                return memberIntroduction.Substring(0, memberIntroduction.Length - 1) + " of class " + classNumber + ".";
+            }
+            else if (person is Student)
+            {
+                students.Add((Student)person);
+                foreach (var student in students)
+                {
+                    var memberIntroduction = student.Introduce();
+                    return memberIntroduction.Substring(0, memberIntroduction.Length - 1) + " of class " + classNumber + ".";
+                }
+            }
+
+            return "invalid input";
+        }
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
         }
     }
 }
--- OOStepByStepTest/ClassTest.cs
+++ OOStepByStepTest/ClassTest.cs
@@ -9,9 +9,9 @@ namespace OOStepByStepTest
         public void Should_return_msg_when_Introduce_given_name_age_class_of_teacher()
         {
             Teacher teacher = new Teacher("Amy", 30);
-            var classobj = new Class(teacher);
+            var classobj = new Class(2);
 
-            string msg = classobj.ClassMember(teacher, 2);
+            string msg = classobj.ClassMember(teacher);
 
             Assert.Equal("My name is Amy. I am 30 years old. I am a teacher of class 2.", msg);
         }
@@ -19,12 +19,14 @@ namespace OOStepByStepTest
         [Fact]
         public void Should_return_msg_when_Introduce_given_name_age_class_of_student()
         {
-            Student student = new Student("Amy", 30);
-            var classobj = new Class(student);
+            Student student1 = new Student("Tom", 21);
+            Student student2 = new Student("Eleanor", 23);
+            var classobj = new Class(3);
+            classobj.AddStudent(student1);
 
-            string msg = classobj.ClassMember(student, 3);
+            string msg = classobj.ClassMember(student2);
 
-            Assert.Equal("My name is Amy. I am 30 years old. I am a student of class 3.", msg);
+            Assert.Equal("My name is Tom. I am 21 years old. I am a student of class 3.", msg);
         }
     }
 }
