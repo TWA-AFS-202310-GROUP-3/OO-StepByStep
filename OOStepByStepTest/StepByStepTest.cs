bd438055073932f13806fffb5ee9c19973fe1b33
Pengyu <PChen10@slb.com>
2023-11-02 20:53:25 +0800

    feat: finish AC1 with UT seccesed

---
+++ OOStepByStepTest/StepByStepTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+using OOStepByStep;
+namespace OOStepByStepTest
+{
+    public class StepByStepTest
+    {
+        [Fact]
+        public void Should_return_introduction_when_given_person_info()
+        {
+            var person = new Person("Pengyu", 25);
+            string msg = person.SelfIntroduce();
+
+            Assert.Equal("My name is Pengyu. I am 25 years old", msg);
+        }
+    }
+}
