b4dfb6c3805b662fd2d205465b072eb9b7b74daa
Shuhan Jin <sjin9@slb.com>
2023-11-02 17:03:28 +0800

    feat: create person class that can introduce themselves with name and age

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOStepByStep
+{
+    public class Person
+    {
+        private string name = "Tom";
+        private int age = 21;
+
+        public virtual string Introduce()
+        {
+            return $"My name is {name}. I am {age} years old.";
+        }
+    }
+}
---
+++ OOStepByStepTest/PersonTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using OOStepByStep;
+
+namespace OOStepByStepTest
+{
+    public class PersonTest
+    {
+        [Fact]
+        public void Should_return_msg_when_Introduce_given_name_age()
+        {
+            var person = new Person();
+
+            string msg = person.Introduce();
+
+            Assert.Equal("My name is Tom. I am 21 years old.", msg);
+        }
+    }
+}
