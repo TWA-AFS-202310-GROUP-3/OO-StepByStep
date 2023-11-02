b464d2411561988a5df873b1b3dda83d39ae17af
Ke <kwang97@slb.com>
2023-11-02 21:14:42 +0800

    feat: implement partial class Classroom and IntroduceSelf method

---
+++ OOStepByStep/ClassRoom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOStepByStep
+{
+    public partial class ClassRoom
+    {
+        private int number;
+        private Teacher teacher;
+        private List<Student> students;
+
+        public ClassRoom(int number)
+        {
+            this.number = number;
+        }
+
+        public string IntroduceSelf(Person person)
+        {
+            var selfIntroductionWithoutClassNumber = person.SelfIntroduction();
+            return selfIntroductionWithoutClassNumber.Substring(0, selfIntroductionWithoutClassNumber.Length - 1) + $" of class {this.number}";
+        }
+    }
+}
--- OOStepByStepTest/ClassroomTest.cs
+++ OOStepByStepTest/ClassroomTest.cs
@@ -9,5 +9,16 @@ namespace OOStepByStepTest
 
     public class ClassroomTest
     {
+        [Fact]
+        public void Should_return_selfIntroduction_When_classRoom_selfIntroduction_Given_person()
+        {
+            var expect = "My name is Tom. I am 25 years old. I am a student of class 2";
+
+            Person student = new Student("Tom", 25);
+            ClassRoom classRoom = new ClassRoom(2);
+            var actual = classRoom.IntroduceSelf(student);
+
+            Assert.Equal(expect, actual);
+        }
     }
 }
