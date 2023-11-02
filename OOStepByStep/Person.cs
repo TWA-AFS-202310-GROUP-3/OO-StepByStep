967b66acded4608a9d3a7b033fb7afa3992590fc
Wang Jing <jwang315@slb.com>
2023-11-02 16:53:21 +0800

    feat: implement Teacher and add test for teacher introduction.

--- OOStepByStep/Person.cs
+++ OOStepByStep/Person.cs
@@ -33,4 +33,16 @@ namespace OOStepByStep
             return base.IntroduceSelf() + "I am a student. ";
         }
     }
+
+    public class Teacher : Person
+    {
+        public Teacher(string name, int age) : base(name, age)
+        {
+        }
+
+        public override string IntroduceSelf()
+        {
+            return base.IntroduceSelf() + "I am a teacher. ";
+        }
+    }
 }
--- OOStepByStepTest/PersonTest.cs
+++ OOStepByStepTest/PersonTest.cs
@@ -26,10 +26,22 @@ namespace OOStepByStepTest
         public void Should_return_name_and_age_and_student_when_introduce()
         {
             // Given
-            Student student = new Student("Tom", 21);
-            string expectedResult = "My name is Tom. I am 21 years old. I am a student. ";
+            Student student = new Student("Tom", 18);
+            string expectedResult = "My name is Tom. I am 18 years old. I am a student. ";
             // When
             string result = student.IntroduceSelf();
+            // Then
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Should_return_name_and_age_and_teacher_when_introduce()
+        {
+            // Given
+            Teacher teacher = new Teacher("Amy", 30);
+            string expectedResult = "My name is Amy. I am 30 years old. I am a teacher. ";
+            // When
+            string result = teacher.IntroduceSelf();
             // Then
             Assert.Equal(expectedResult, result);
         }
