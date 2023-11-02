4b7fdf8a0e185f851dba94ef5a5d017514384724
Ke <kwang97@slb.com>
2023-11-02 23:28:26 +0800

    feat: implement JoinIn method for Classroom, and Joinin should return welcomed selfIntroductions from the person in the classroom when a new person join in the classroom

--- OOStepByStep/ClassRoom.cs
+++ OOStepByStep/ClassRoom.cs
@@ -21,7 +21,7 @@ namespace OOStepByStep
         {
             if (person is Teacher)
             {
-                if (this.teacher == null || IsThePerson(this.teacher, person))
+                if (this.teacher == null || !IsThePerson(this.teacher, person))
                 {
                     return string.Empty;
                 }
@@ -34,27 +34,87 @@ namespace OOStepByStep
                     return string.Empty;
                 }
 
-                bool found = false;
-                for (int i = 0; i < this.students.Count; i++)
-                {
-                    var aStudent = this.students[i];
-                    if (IsThePerson(aStudent, person))
-                    {
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (!found) { return string.Empty; }
+                if (!IncludesStudent(person)) { return string.Empty; }
             }
 
             var selfIntroductionWithoutClassNumber = person.SelfIntroduction();
-            return selfIntroductionWithoutClassNumber.Substring(0, selfIntroductionWithoutClassNumber.Length - 1) + $" of class {this.number}";
+            return selfIntroductionWithoutClassNumber.Substring(0, selfIntroductionWithoutClassNumber.Length - 1) + $" of class {this.number}.";
+        }
+
+        public List<string> JoinIn(Person person)
+        {
+            List<string> result = new List<string>();
+            if (person is Teacher)
+            {
+                if (!(this.teacher is null))
+                {
+                    return null;
+                }
+
+                this.teacher = new Teacher(person.Name, person.Age);
+                if (this.students != null)
+                {
+                    for (int i = 0; i < this.students.Count; i++)
+                    {
+                        result.Add(Welcome(this.students[i], person));
+                    }
+                }
+            }
+            else if (person is Student)
+            {
+                if (this.students == null)
+                {
+                    this.students = new List<Student>();
+                }
+
+                if (IncludesStudent(person))
+                {
+                    return null;
+                }
+
+                if (this.teacher != null)
+                {
+                    result.Add(Welcome(this.teacher, person));
+                }
+
+                if (this.students != null)
+                {
+                    for (int i = 0; i < this.students.Count; i++)
+                    {
+                        result.Add(Welcome(this.students[i], person));
+                    }
+                }
+
+                this.students.Add(person as Student);
+            }
+
+            return result;
         }
 
         public bool IsThePerson(Person p1, Person p2)
         {
             return p1.Name == p2.Name && p1.Age == p2.Age;
+        }
+
+        public bool IncludesStudent(Person student)
+        {
+            bool found = false;
+            for (int i = 0; i < this.students.Count; i++)
+            {
+                var aStudent = this.students[i];
+                if (IsThePerson(aStudent, student))
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            return found;
+        }
+
+        public string Welcome(Person welcomer, Person welcomee)
+        {
+            return $"{IntroduceSelf(welcomer)} Welcome {welcomee.Name} join class {this.number}.";
         }
     }
 }
--- OOStepByStepTest/ClassroomTest.cs
+++ OOStepByStepTest/ClassroomTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 namespace OOStepByStepTest
 {
     using OOStepByStep;
+    using System.Collections.Generic;
     using Xunit;
 
     public class ClassroomTest
@@ -12,12 +13,28 @@ namespace OOStepByStepTest
         [Fact]
         public void Should_return_selfIntroduction_When_classRoom_selfIntroduction_Given_person()
         {
-            var expect = "My name is Tom. I am 25 years old. I am a student of class 2";
+            var expect = "My name is Tom. I am 25 years old. I am a student of class 2.";
 
             Person student = new Student("Tom", 25);
             ClassRoom classRoom = new ClassRoom(2);
+            classRoom.JoinIn(student);
             var actual = classRoom.IntroduceSelf(student);
 
+            Assert.Equal(expect, actual);
+        }
+
+        [Fact]
+        public void Should_return_welcome_selfIntroductions_When_classRoom_JoinIn_Given_person()
+        {
+            List<string> expect = new List<string>{ "My name is Amy. I am 30 years old. I am a teacher of class 2. Welcome Jim join class 2.",
+                                        "My name is Tom. I am 18 years old. I am a student of class 2. Welcome Jim join class 2." };
+            Person jim = new Student("Jim", 25);
+            Person amy = new Teacher("Amy", 30);
+            Person tom = new Student("Tom", 18);
+            ClassRoom classRoom = new ClassRoom(2);
+            classRoom.JoinIn(amy);
+            classRoom.JoinIn(tom);
+            var actual = classRoom.JoinIn(jim);
             Assert.Equal(expect, actual);
         }
     }
