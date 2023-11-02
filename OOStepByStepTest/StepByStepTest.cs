56ded6d0104a85003b28a480d43a75706551478b
Kailin Liu <KLiu13@slb.com>
2023-11-02 17:23:21 +0800

    feat: creat student and teacher class than can introduce different itself

--- OOStepByStepTest/StepByStepTest.cs
+++ OOStepByStepTest/StepByStepTest.cs
@@ -22,5 +22,31 @@ namespace OOStepByStepTest
             //Then
             Assert.Equal("My name is Tom. I am 21 years old.", introduction);
         }
+
+        [Fact]
+        public void Should_return_name_and_age_with_a_student()
+        {
+            //Given
+            Student student = new Student("Tom", 18);
+
+            //When
+            string introduction = student.Introduce();
+
+            //Then
+            Assert.Equal("My name is Tom. I am 18 years old. I am a student.", introduction);
+        }
+
+        [Fact]
+        public void Should_return_name_and_age_with_a_teacher()
+        {
+            //Given
+            Teacher teacher = new Teacher("Amy", 30);
+
+            //When
+            string introduction = teacher.Introduce();
+
+            //Then
+            Assert.Equal("My name is Amy. I am 30 years old. I am a teacher.", introduction);
+        }
     }
 }
