648110caa4ff096040f1fbb7624986bb6d1a9aa1
Ke <kwang97@slb.com>
2023-11-02 23:33:04 +0800

    fix: mock the code extensible development, using the partial class of the classroom in order to avoid influence the code in classroom.cs

--- OOStepByStep/ClassRoom.cs
+++ OOStepByStep/ClassRoom.cs
@@ -41,56 +41,6 @@ namespace OOStepByStep
             return selfIntroductionWithoutClassNumber.Substring(0, selfIntroductionWithoutClassNumber.Length - 1) + $" of class {this.number}.";
         }
 
-        public List<string> JoinIn(Person person)
-        {
-            List<string> result = new List<string>();
-            if (person is Teacher)
-            {
-                if (!(this.teacher is null))
-                {
-                    return null;
-                }
-
-                this.teacher = new Teacher(person.Name, person.Age);
-                if (this.students != null)
-                {
-                    for (int i = 0; i < this.students.Count; i++)
-                    {
-                        result.Add(Welcome(this.students[i], person));
-                    }
-                }
-            }
-            else if (person is Student)
-            {
-                if (this.students == null)
-                {
-                    this.students = new List<Student>();
-                }
-
-                if (IncludesStudent(person))
-                {
-                    return null;
-                }
-
-                if (this.teacher != null)
-                {
-                    result.Add(Welcome(this.teacher, person));
-                }
-
-                if (this.students != null)
-                {
-                    for (int i = 0; i < this.students.Count; i++)
-                    {
-                        result.Add(Welcome(this.students[i], person));
-                    }
-                }
-
-                this.students.Add(person as Student);
-            }
-
-            return result;
-        }
-
         public bool IsThePerson(Person p1, Person p2)
         {
             return p1.Name == p2.Name && p1.Age == p2.Age;
@@ -112,9 +62,5 @@ namespace OOStepByStep
             return found;
         }
 
-        public string Welcome(Person welcomer, Person welcomee)
-        {
-            return $"{IntroduceSelf(welcomer)} Welcome {welcomee.Name} join class {this.number}.";
-        }
     }
 }
